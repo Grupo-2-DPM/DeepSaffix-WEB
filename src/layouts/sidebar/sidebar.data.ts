232066c99dc1b33de1296cd629d0870c54e34544
Daniel Alejandro Lasso Molina <lassomoluna9@gmail.com>
2026-02-27 17:51:05 -0500

    fix: change profile url to perfil url

--- src/layouts/sidebar/sidebar.data.ts
+++ src/layouts/sidebar/sidebar.data.ts
@@ -1,6 +1,6 @@
 export const NAV_LINKS = [
   { label: 'Inicio', href: '#/' },
-  { label: 'Perfil', href: '#/profile' },
+  { label: 'Perfil', href: '#/perfil' },
   // { label: 'Historial', href: '#/simulacro' },
   { label: 'Overview', href: '#/overview' },
   { label: 'Saber Mas', href: '#/saber-mas' },
